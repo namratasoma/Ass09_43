76de96f120134a8796b43c6c6aff12f10ec3ccaf
namratasoma <100586510+namratasoma@users.noreply.github.com>
2023-06-02 20:23:02 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+    public static void main(String cp[])
+     {  
+        System.ut.println("Hello Everyone!!");
+        System.out.println("I am Namrata Soma");
+     }
+ }
+      
