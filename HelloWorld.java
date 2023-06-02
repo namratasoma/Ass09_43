5fed8a365ff41ecbd7fe1eab1c24728c5f635369
namratasoma <100586510+namratasoma@users.noreply.github.com>
2023-06-02 20:52:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
     public static void main(String cp[])
      {  
-        System.ut.println("Hello Everyone!!");
+        System.out.println("Hello Everyone!!");
         System.out.println("I am Namrata Soma");
      }
  }
